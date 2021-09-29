174efc08152907b41ff7bf6d36351c02dce69a3a
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-09-28 18:41:26 -0800

    boxer.cpp has attempt at astericks box

---
+++ boxprint/boxer.cpp
@@ -0,0 +1,35 @@
+//boxer.cpp
+//Lisa Jacklin
+//CS 201
+//9/25/2021
+
+#include<iostream>
+using namespace std;
+
+//defines createBox and makes an integer the bounds for the box
+int createBox() {
+    int num;
+
+for (int a = 1; a <= num; a++)
+{
+    cout << "*";
+}
+cout << endl;
+for (int b = 1; b <= 5; b++)
+{
+    for (int c = 1; c <= 1; c++)
+    {
+        cout << "*";
+    }
+    for (int d = 1; d <= 8; d++)
+    {
+        cout << " ";
+    }
+    cout << "*" << endl;
+}
+for (int e = 1; e <= 10; e++)
+{
+    cout << "*";
+}
+return 0;
+}
---
+++ boxprint/boxer.h
@@ -0,0 +1,4 @@
+#ifndef BOXER_H
+#define BOXER_H
+
+#endif
