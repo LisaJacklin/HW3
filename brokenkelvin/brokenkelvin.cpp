1b3a8bddac0a58add71a1f86832c14c5dbcaa6d3
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-09-28 21:59:45 -0800

    corrected code

--- brokenkelvin/brokenkelvin.cpp
+++ brokenkelvin/brokenkelvin.cpp
@@ -2,12 +2,31 @@
 // Lisa Jacklin
 // CS 201
 // 9/26/2021
-//
+//requirement: list the errors and correct the code
 
+//code needs to include iostream and using std::cout and std::cin
 #include <iostream>
+
+using std::cout;
+using std::cin;
+
+//Converts Celsius to Kelvin
+double ctok(double c) 
+{
+	//cannot go from a double to an int so int k is changed to double k
+	double k = c + 273.15;
+	//cannot return a type(double), must return a value which is k.
+	return k;
+}
 
 int main()
 {
- 
-}
+	double c = 0;
+	//what is d? d is not an integer that is defined so I changed it to c.
+	cin >> c;
+	//
+	double k = ctok(c);
 
+	//cout has to be lower case, capital C is changed
+	cout << k << '\n';
+}
