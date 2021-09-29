66b02f2e07d108846b8c3a6a1fad51af063aa883
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-09-28 19:22:33 -0800

    simple one layer box can be created successfully

--- boxprint/boxer.cpp
+++ boxprint/boxer.cpp
@@ -9,25 +9,31 @@ using namespace std;
 //defines createBox and makes an integer the bounds for the box
 int createBox(int value) {
 
-
-    for (int a = 1; a <= value; a++)
+ //creates the top row of the box
+ for (int a = 1; a <= 10; a++)
     {
     cout << "*";
     }
 cout << endl;
-for (int b = 1; b <= value; b++)
+
+// total amount of rows minus the top and botom rows
+for (int b = 1; b <= 8; b++)
 {
-    for (int c = 1; c <= value; c++)
+    //outside left and right sides and how many astericks that are on the outline
+    for (int c = 1; c <= 1; c++)
     {
         cout << "*";
     }
-    for (int d = 1; d <= value; d++)
+
+    //the amount of spaces between the the astericks on the left and on the right
+    for (int d = 1; d <= 10; d++)
     {
         cout << " ";
     }
     cout << "*" << endl;
 }
-for (int e = 1; e <= value; e++)
+//creates the bottom row
+for (int e = 1; e <= 10; e++)
 {
     cout << "*";
 }
