5765d35d0e631d2f110e5ba893a4a4eed6aa991a
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-09-28 18:44:57 -0800

    astericks box prototype and function created and compiling

--- boxprint/boxer.cpp
+++ boxprint/boxer.cpp
@@ -7,27 +7,26 @@
 using namespace std;
 
 //defines createBox and makes an integer the bounds for the box
-int createBox() {
-    int num;
+int createBox(int num) {
 
 for (int a = 1; a <= num; a++)
 {
     cout << "*";
 }
 cout << endl;
-for (int b = 1; b <= 5; b++)
+for (int b = 1; b <= num; b++)
 {
-    for (int c = 1; c <= 1; c++)
+    for (int c = 1; c <= num; c++)
     {
         cout << "*";
     }
-    for (int d = 1; d <= 8; d++)
+    for (int d = 1; d <= num; d++)
     {
         cout << " ";
     }
     cout << "*" << endl;
 }
-for (int e = 1; e <= 10; e++)
+for (int e = 1; e <= num; e++)
 {
     cout << "*";
 }
--- boxprint/boxer.h
+++ boxprint/boxer.h
@@ -1,4 +1,8 @@
 #ifndef BOXER_H
 #define BOXER_H
 
+
+//prototype of box of astericks
+int createBox();
+
 #endif
